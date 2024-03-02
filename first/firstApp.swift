b5c2f2f4780d84bb8d6e2d431f8a690e050c276a
Charles Owen <owen.charlesr@hotmail.com>
2024-03-02 00:29:37 -0500

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by Charles R. Owen on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
